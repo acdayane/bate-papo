4715843230132dc788e6bef2d59202bb28047242
Dayane <dayaneac@hotmail.com>
2022-08-26 16:51:22 -0300

    versao com layout, chat e entrada na sala

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
 let nomeUsuario = '';
 let chat = [];
-let linhaChat = '';
 
 entrarNaSala();
 
-function entrarNaSala() {
-    nomeUsuario = prompt ('Digite seu lindo nome:');
+// entrada na sala
 
-    enviarUsuario();
-    setInterval(importarChat,3000);
+function entrarNaSala() {
+    const usuario = prompt ('Digite seu lindo nome:');
+    nomeUsuario = {
+        name: usuario,
+    }
+    enviarUsuario();    
 }
 
+function enviarUsuario() {
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', nomeUsuario);
+    promessa.then(tratarSucesso);
+    promessa.catch(tratarErro); 
+}
 
+function tratarSucesso(res){
+    console.log ('Deu certo o envio do usuário :)', res)
+    setInterval(importarChat,3000);
+    setInterval(manterConexao, 5000);
+}
 
-function importarChat () {
+function tratarErro(err){
+    console.log ('Deu ruim o envio do usuário :(', err)
+    console.log(err.response.status);
+    if (err.response.status === 400){
+        alert ('O nome já está sendo usado. Tente outro ;)');
+        entrarNaSala();
+    }
+}
+
+function manterConexao() {
+    const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/status', nomeUsuario);
+    promessa.then(console.log('Conexão mantida por mais 5s'));    
+    promessa.catch(deuErroConexao); 
+}
+
+function deuErroConexao(err) {
+    console.log('Deu erro na manutenção da conexão', err);
+    alert("Desconectado após 5s de inatividade");   
+}
+
+// chat
+
+function importarChat() {
     const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
     promessa.then(renderizarChat);
     promessa.catch(deuErro);
@@ -23,7 +57,7 @@ function importarChat () {
 }
 
 function renderizarChat(res){
-    console.log('deu certo');
+    console.log('Deu certo o recebimento das mensagens', res);
     //console.log(res.data);
     const chatSemFiltro = res.data; 
 
@@ -36,8 +70,7 @@ function renderizarChat(res){
         const listaChat = document.querySelector('.lista-chat');
 
         const chat = chatSemFiltro.filter(msgVisivel);
-        console.log(chat);
-
+     
         for (let i = 0; i < chat.length; i++) {
             listaChat.innerHTML += `
             <li class = "linha-chat ${chat[i].type}"> 
@@ -48,109 +81,12 @@ function renderizarChat(res){
                 <p>${chat[i].text}</p>        
             </li>
           `;
-
-    }   
+        }   
 }
-
 
 function deuErro(err) {
-    console.log('deu erro');
-    alert("Algo deu errado. Tenta de novo ;)");
+    console.log('Deu erro no recebimento das mensagens', err);
+    alert("Algo deu errado. Tenta de novo ;)");   
 }
 
-/*
-entrarNaSala();
-
-
-
-function entrarNaSala() {
-    const novousuario = prompt ('Digite seu lindo nome:');
-    participantes = {
-        name: novousuario,
-    }
-
-    console.log(participantes);
-    enviarDados();
-
-}
-
-
-
-function pegarDados (){
-    const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
-    promessa.then(dadosChegaram);
-    promessa.catch(deuErro);
-}
-
-function dadosChegaram(res){
-    console.log("Resposta completa do get", res);
-    console.log("Resposta.data do get", res.data);
-} 
-
-function enviarDados (){
-
-    // enviar novousuario para o servidor
-    const promessa = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', participantes);
-    promessa.then(deuCerto);
-    promessa.catch(deuErro);
-    
- 
-    // renderizar novousuario no html
-   // renderizarUsuarios();
-}
-
-function deuCerto(res){ // pegar dados
-
-    console.log('Deu certo!');
-    console.log(res);
-    const promessa = axios.get ('https://mock-api.driven.com.br/api/v6/uol/participants');
-    promessa.then(dadosChegaram);
-
-    renderizarUsuarios();
-}
-
-function deuErro(err){
-    console.log('Deu erro :(');
-    console.log(err.response.status);
-    if (err.response.status === 400){
-        alert ('O nome já está sendo usado. Tente outro ;)');
-        entrarNaSala();
-    }
-}
-
-
-function renderizarUsuarios(){
-    const chat = document.querySelector('.ul');
-    chat.innerHTML = "";
-
-    for (let i=0; i<listaParticipantes.lenght; i++){
-        innerHTML += `
-        <li> ${participantes = {
-            name: novousuario,
-        }}
-        <li>
-        `
-    }
-
-}
-
-
-function carregarMensagens {
-    const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/participants');
-
-    promessa.then (quandoSucesso);
-    console.log('Deu certo')
-    promessa.then (quandoErro);
-    console.log ('Deu ruim')
-}
-
-function manterConexao() {
-
-}
-
-function buscarMensagens() {
-
-}
-
-function enviarMensagem() {
-    const mensagem = document.querySelector('.mensagem'); */
+// envio de mensagem
